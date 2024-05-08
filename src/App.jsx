827504ac2e312a92bb7a9cb005fa6c54f9bf4b18
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-08 23:10:17 +0530

    app ,jsx running

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
 
     <div>
       <Navbar/>
-      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<video/>} />
+      </Routes>
+
 
     </div>
   )

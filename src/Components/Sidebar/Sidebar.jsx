639475149ac4af0e54f8becde36893cc6aba4cf0
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-22 01:09:23 +0530

    Sidebar.css running

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -18,7 +18,7 @@ import cameron from '../../assets/cameron.png'
 const Sidebar = ({sidebar}) => {
   return (
 
-    <div className={'sidebar ${sidebar? "":"small-sidebar}'}>
+    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
     <div className="sortcut-links">
         <div className="side-link">
             <img src={home} alt="" /><p>Home</p>

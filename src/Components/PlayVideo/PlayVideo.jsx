f685dcd26a41cb30a144ae0ff3e2fbcc3ac10a96
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-26 21:17:59 +0530

    src video running

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './PlayVideo.css'
+import video1 from '../../assets/Video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
 
 const PlayVideo = () => {
     return(
-        <div>
+        <div className='play-video'>
+            <video src={video1}></video>
 
         </div>
     )
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 
 const Video = () => {
   return (
 
     <div className='play-container'>
+      <PlayVideo/>
 
     </div>
   )
